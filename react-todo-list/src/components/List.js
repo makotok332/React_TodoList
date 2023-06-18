0cbbec42eba27100620f9885de7e8202f66c41b8
木村誠 <>
2023-06-19 02:59:13 +0900

    検索機能追加

--- react-todo-list/src/components/List.js
+++ react-todo-list/src/components/List.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 const List = ({ todos, deleteTodo }) => {
   const complete = (id) => {
     deleteTodo(id);
   };
 
+  const [filterVal, setFilterVal] = useState("");
+
   return (
     <div>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <button onClick={() => complete(todo.id)}>完了</button>
-            <span>{todo.content}</span>
-          </div>
-        );
-      })}
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => setFilterVal(e.target.value)}
+        placeholder="Search Keyword"
+      />
+      {todos
+        .filter((todo) => todo.content.indexOf(filterVal) !== -1)
+        .map((todo) => {
+          return (
+            <div key={todo.id}>
+              <span>{todo.content}</span>
+              <button onClick={() => complete(todo.id)}>完了</button>
+            </div>
+          );
+        })}
     </div>
   );
 };
